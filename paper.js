6e69aed6f0775fc51579aef35f810e8563264a45
Akshat-I <75306625+Akshat-I@users.noreply.github.com>
2021-04-26 12:34:36 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+
+    constructor(x,y,r){
+
+        var options = {
+
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0,
+            density: 1.2
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+          display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0,0,this.r);
+        pop();
+      }
+}
